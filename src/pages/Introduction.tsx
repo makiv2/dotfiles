5b69b1382166fd8e01893daf3eafe0439f97f995
Matija Popovic <matijapopovic001212@gmail.com>
2023-10-18 21:21:56 +0200

    init Overview page

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,11 +1,15 @@
+import '../styles/introduction.css'
 
+const introText = "Welcome to the ultimate destination for Mac enthusiasts and users seeking to optimize their computing experience. Our website is your comprehensive guide to all things Mac setup and configuration. Whether you're a seasoned Mac aficionado looking to fine-tune your existing setup or a newcomer eager to embark on your Apple journey, we've got you covered. Explore a wealth of expert insights, tips, and tutorials that will empower you to unlock the full potential of your Mac, creating a customized, efficient, and seamless computing environment tailored to your unique needs. Dive into the world of macOS and discover the art of configuring your Mac to perfection, all right here on our website.";
 
 const Introduction = () => {
-
     return (
-        <>
-            This is a test page
-        </>
+        <div className={"intro"}>
+            <div className={"intro__div"}>
+                <h1>Welcome to the Ultimate Mac Setup and Configuration Guide!</h1>
+                <p>{introText}</p>
+            </div>
+        </div>
     )
 }
 
