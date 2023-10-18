64a193e2c485d9537559e4b74b65ebb4d4f9d08c
Matija Popovic <matijapopovic001212@gmail.com>
2023-10-18 21:30:27 +0200

    init system page

--- src/pages/System.tsx
+++ src/pages/System.tsx
@@ -2,7 +2,14 @@ const System = () => {
     return (
         <>
             <div>
-                <h1>The system page</h1>
+                <h1>System Settings</h1>
+                <h2>Trackpad</h2>
+                <h2>Dock</h2>
+                <h2>Finder</h2>
+                <h2>Menubar</h2>
+                <h2>Spotlight</h2>
+                <h2>Accounts</h2>
+                <h2>User Defaults</h2>
             </div>
         </>
     )
