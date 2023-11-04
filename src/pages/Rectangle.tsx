26343edff33ef674ff00aaece05c2b9e1b4f34e6
Matija Popovic <matijapopovic001212@gmail.com>
2023-11-04 12:54:53 +0100

    system Settings

---
+++ src/pages/Rectangle.tsx
@@ -0,0 +1,11 @@
+import '../styles/rectangle.css'
+
+
+const Introduction = () => {
+    return (
+        <div className={"rectangle "}>
+        </div>
+    )
+}
+
+export default Introduction;
--- src/pages/System.tsx
+++ src/pages/System.tsx
@@ -1,30 +1,70 @@
+import '../styles/system.css'
+
 const System = () => {
     return (
-        <div>
-            <h1>System Settings</h1>
+        <div className={"system"}>
+            <div className={"system__div"}>
+                <h1>System Settings</h1>
 
-            <h2>Trackpad</h2>
-            <ul>
-                <li>Point & Click
-                    <ul>
-                        <li>Disable force click and haptic feedback</li>
-                        <li>Enable tap to click</li>
-                    </ul>
-                </li>
-                <li>Scroll & Zoom
-                    <ul>
-                        <li>Check all</li>
-                    </ul>
-                </li>
-            </ul>
+                <h2>Trackpad</h2>
+                <ul>
+                    <li>Point & Click
+                        <ul>
+                            <li>Disable force click and haptic feedback</li>
+                            <li>Enable tap to click</li>
+                        </ul>
+                    </li>
+                    <li>Scroll & Zoom
+                        <ul>
+                            <li>Check all</li>
+                        </ul>
+                    </li>
+                </ul>
 
-            <h2>Dock</h2>
-            <ul></ul>
-            <h2>Finder</h2>
-            <h2>Menubar</h2>
-            <h2>Spotlight</h2>
-            <h2>Accounts</h2>
-            <h2>User Defaults</h2>
+                <h2>Dock</h2>
+                <ul>
+                    <li>Visual Settings
+                        { /* Picture of sliders */}
+                    </li>
+                </ul>
+
+                <h2>Finder</h2>
+                <ul>
+                    <li>General
+                        <ul>
+                            <li>Change new widow to home folder</li>
+                        </ul>
+                    </li>
+                    <li>Sidebar
+                        <ul>
+                            <li>
+                                Add home folder to sidebar
+                            </li>
+                            <li>
+                               Add repos folder to sidebar
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <h2>Spotlight</h2>
+                <ul>
+                    <li>
+                        Remove and replace with Alfred
+                    </li>
+                </ul>
+
+                <h2>Defaults</h2>
+                <ul>
+                    <li>
+                        Enable repeating keys: defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+                    </li>
+                    <li>
+                        Change default screenshot folder: defaults write com.apple.screencapture location /path/to/screenshots/ && killall SystemUIServer
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
