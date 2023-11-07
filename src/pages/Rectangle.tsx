ee0211115fd13e33469d0b256cab56982932cefd
Matija Popovic <matijapopovic001212@gmail.com>
2023-11-07 13:10:09 +0100

    rectangle demo

--- src/pages/Rectangle.tsx
+++ src/pages/Rectangle.tsx
@@ -4,8 +4,11 @@ import '../styles/rectangle.css'
 const Rectangle = () => {
     return (
         <div className={"rectangle "}>
-            <h1>Rectangle</h1>
-            <p>Move and resize windows in macOS using keyboard shortcuts or snap areas, download from <a href={"https://rectangleapp.com/"}>here</a> </p>
+            <div className={"rectangle_div"}>
+                <h1>Rectangle</h1>
+                <p>Move and resize windows in macOS using keyboard shortcuts or snap areas, download from <a
+                    href={"https://rectangleapp.com/"}>here</a></p>
+            </div>
         </div>
     )
 }
