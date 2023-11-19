7d95bb773e0e13c428fd6a94fc0bd8a3a8d69c3f
Matija Popovic <matijapopovic001212@gmail.com>
2023-11-19 23:54:54 +0100

    small changes

--- src/pages/Rectangle.tsx
+++ src/pages/Rectangle.tsx
@@ -5,9 +5,11 @@ const Rectangle = () => {
     return (
         <div className={"general"}>
             <div className={"general__div"}>
+                <hr className={"line__top"}></hr>
                 <h1>Rectangle</h1>
                 <p>Move and resize windows in macOS using keyboard shortcuts or snap areas, download from <a
                     href={"https://rectangleapp.com/"}>here</a></p>
+                <hr className={"line__bottom"}></hr>
             </div>
         </div>
     )
